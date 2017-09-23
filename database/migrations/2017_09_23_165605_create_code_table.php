076e86102f7e413558e0e06d61b6692d05bd24a5
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-09-24 01:59:18 +0900

    migration

---
+++ database/migrations/2017_09_23_165605_create_code_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('code', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("title");
+            $table->string("URL");
+            $table->text("comment");
+            $table->integer("user_id");
+            $table->integer("subject_id");
+            $table->integer("is_company_id")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('code');
+    }
+}
